20d177bd1f80bbfef84723997060140d483414f1
Adam Shalala <adamshalala@Adams-Air.localdomain>
2023-07-06 12:56:22 +1000

    updated github component to include images from unsplash based off title of the repo

--- src/components/Github.js
+++ src/components/Github.js
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from 'react';
-
 import { Col } from 'react-bootstrap';
-
-
 
 const Github = ({ username }) => {
   const [repos, setRepos] = useState([]);
+  const [imageUrls, setImageUrls] = useState([]);
 
   useEffect(() => {
     const fetchRepos = async () => {
@@ -21,13 +19,42 @@ const Github = ({ username }) => {
     fetchRepos();
   }, [username]);
 
+  useEffect(() => {
+    const fetchUnsplashImages = async () => {
+      const urls = await Promise.all(
+        repos.map(async (repo) => {
+          try {
+            const response = await fetch(
+              `https://api.unsplash.com/search/photos?page=1&query=${encodeURIComponent(repo.name)}&client_id=PPmNV3IiGnmcsezouaLWcwAj_u9ZQiQd0NTsNtkcsM0`
+            );
+            const data = await response.json();
+            if (data.results.length > 0) {
+              return data.results[0].urls.small;
+            } else {
+              return null;
+            }
+          } catch (error) {
+            console.error('Error fetching Unsplash image:', error);
+            return null;
+          }
+        })
+      );
+      setImageUrls(urls);
+    };
+
+    fetchUnsplashImages();
+  }, [repos]);
+
   return (
     <div>
-      <h2>Check Out My Latest Repos</h2>
+     
       <div className="row">
         {repos.map((repo, index) => (
-          <Col key={repo.id} xs={12} md={6} lg={4} className={`my-3 ${index % 2 === 0 ? 'even-col' : ''}`}>
-            <a href={repo.html_url} target="_blank" rel="noopener noreferrer">
+          <Col key={repo.id} xs={12} md={6} lg={4} className="my-3">
+            {imageUrls[index] && (
+              <img src={imageUrls[index]} alt={repo.name} className="img-fluid" />
+            )}
+            <a href={repo.html_url} target="_blank" rel="noopener noreferrer" className="text-decoration-none">
               {repo.name}
             </a>
           </Col>
